db41eb67260326667fae54e349bbc19e40b3b4c5
quocln <quocln@me.com>
2023-05-31 13:08:08 +0700

    Implement shell script auto generate

--- setup-infras.sh
+++ setup-infras.sh
@@ -1,4 +1,10 @@
 DIR="./.docker"
+FILE_NGINX="./.docker/nginx.conf.dist"
+FILE_PHP="./.docker/php.ini.dist"
+FILE_DOCKER_COMPOSE="./docker-composer.yml"
+FILE_MAKEFILE="./Makefile"
+DOWNLOAD_URL="https://raw.githubusercontent.com/cezlab/setup-docker-utility/main"
+
 echo "#1 - Check Folder Docker Configuration"
 if [ -d "$DIR" ]; then
   ### Take action if $DIR exists ###
@@ -7,4 +13,36 @@ else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "${DIR} directory does not exists - Create ${DIR}"
   mkdir $DIR
+fi
+
+echo "#2 - Check ${FILE_DOCKER_COMPOSE} file"
+if test -f "$FILE_DOCKER_COMPOSE"; then
+    echo "$FILE_DOCKER_COMPOSE exists."
+else
+    echo "Download ${FILE_DOCKER_COMPOSE}"
+    curl "${DOWNLOAD_URL}/docker-composer.yml" --output ${FILE_DOCKER_COMPOSE}
+fi
+
+echo "#3 - Check ${FILE_MAKEFILE} file"
+if test -f "$FILE_MAKEFILE"; then
+    echo "$FILE_MAKEFILE exists."
+else
+    echo "Download ${FILE_MAKEFILE}"
+    curl "${DOWNLOAD_URL}/Makefile" --output ${FILE_MAKEFILE}
+fi
+
+echo "#4 - Check ${FILE_NGINX} file"
+if test -f "$FILE_NGINX"; then
+    echo "$FILE_NGINX exists."
+else
+    echo "Download ${FILE_NGINX}"
+    curl "${DOWNLOAD_URL}/.docker/nginx.conf.dist" --output ${FILE_NGINX}
+fi
+
+echo "#5 - Check ${FILE_PHP} file"
+if test -f "$FILE_PHP"; then
+    echo "$FILE_PHP exists."
+else
+    echo "Download ${FILE_PHP}"
+    curl "${DOWNLOAD_URL}/.docker/php.ini.dist" --output ${FILE_PHP}
 fi
